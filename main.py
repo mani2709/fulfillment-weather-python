2ffa0337cceafac0824150c5055b9acbc5261090
mani2709 <45124886+mani2709@users.noreply.github.com>
2018-11-21 15:28:47 +0530

    Update Main.py  for comments only
    updated by manisha for sjsjsjs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License. Manisha is using it
 
 """This is a sample for a weather fulfillment webhook for an Dialogflow agent
 This is meant to be used with the sample weather agent for Dialogflow, located at
